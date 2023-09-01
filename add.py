66b171e18232caad02d128420b40bcb1b5f11601
AaryaDesai1 <143753050+AaryaDesai1@users.noreply.github.com>
2023-09-01 19:19:28 -0400

    Update add.py

--- add.py
+++ add.py
@@ -1,12 +1,12 @@
 def add(a, b):
     """
     Adds two numbers and returns the result.
-    
+
     Args:
-    a (float or int): The first number.
-    b (float or int): The second number.
-    
+        a (float or int): The first number.
+        b (float or int): The second number.
+
     Returns:
-    float or int: The sum of a and b.
+        float or int: The sum of a and b.
     """
     return a + b
